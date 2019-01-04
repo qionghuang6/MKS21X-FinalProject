dbdf2f8361f26189565f7994fbf155b27d823e55
Bryan Lai <bryanjl22@hotmail.com>
2019-01-03 22:30:46 -0500

    Create instance variables for Pokemon.java

---
+++ Pokemon.java
@@ -0,0 +1,10 @@
+public class Pokemon {
+        //Instance Variables:
+        private String name;
+        private String type;
+        private int hp;
+        private int color;
+        private int level;
+        private int exp;
+        //private List<Move> moveset; //Remember to import List.java! 
+}
