5b20a67aa39ebf6da6d795eb6e890fcec0c10d98
Bryan Lai <bryanjl22@hotmail.com>
2019-01-04 09:53:39 -0500

    Rename sidekick to partner, add constructor, accessor methods and mutator methods

--- Player.java
+++ Player.java
@@ -1,8 +1,43 @@
 public class Player extends Pokemon {
         //Instance Variables:
         private Pokemon player;
-        private Pokemon sideKick;
+        private Pokemon partner;
         private int gold;
         //private ArrayList<Pokemon> startingOptions: Starting pokemon the player can choose. Should be defined inside of the main program.
 
+        //Constructor:
+        public Player(String n, String t, int h, int c, int l) {
+                super(n,t,h,c,l);
+        }
+
+        //Accessor methods:
+        public Pokemon getPlayer() {
+                return player;
+        }
+
+        public Pokemon getPartner() {
+                return partner;
+        }
+
+        public Pokemon getGold() {
+                return gold;
+        }
+
+        //Mutator methods:
+        //This method would only be used at the start of the program.
+        public setPlayer(Pokemon p) {
+                player = p;
+        }
+
+        public setPartner(Pokemon p) {
+                partner = p;
+        }
+
+        public addGold(int a) {
+                gold += a;
+        }
+
+        public decreaseGold(int d) {
+                gold -= d;
+        }
 }
