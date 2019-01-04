0ff021973c1ffd9665adeca70868ce1a104e8678
Bryan Lai <bryanjl22@hotmail.com>
2019-01-04 09:48:27 -0500

    Add instance variables for Player.java

---
+++ Player.java
@@ -0,0 +1,8 @@
+public class Player extends Pokemon {
+        //Instance Variables:
+        private Pokemon player;
+        private Pokemon sideKick;
+        private int gold;
+        //private ArrayList<Pokemon> startingOptions: Starting pokemon the player can choose. Should be defined inside of the main program.
+
+}
