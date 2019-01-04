5934c7f8b22cde54e0b93ab32679fc95b3df7714
qionghuang6 <qhuang00@stuy.edu>
2019-01-04 00:29:40 -0500

    added feature to move and build more walkable areas but doesnt work

--- Map.java
+++ Map.java
@@ -22,10 +22,21 @@ public class Map{
     startY = 5;
     int currentX = startX;
     int currentY = startY;
-    buildArea(startX,startY); //makes first area of movable land around start points
+    buildArea(startX,startY,3); //makes first area of movable land around start points
     //moves around building Areas
-    for (int a = 0; a < 5; a++ ) {
-
+    for (int a = 0; a < 10; a++ ) {
+      int lastX = currentX;
+      int lastY = currentY;
+      int ranSize = ((int)(Math.random() * 7));
+      if(Math.random() > 0.5){
+        startX += ranSize;
+      } else{
+        startY += ranSize;
+      }
+      if(!buildArea(currentX,currentY,ranSize -1)){
+        currentX = lastX;
+        currentY = lastY;
+      }
     }
   }
 
@@ -41,16 +52,18 @@ public class Map{
   //Creates a movable area centered at coordinates xS and yS
   //width and length are randomly generated from 3 to 9
   //surrounds moveable area in walls
-  private boolean buildArea(int xS, int yS){
-    int width = (2 * ((int) (Math.random() * 4)) + 3);
-    int length = (2 * ((int) (Math.random() * 4)) + 3);
+  private boolean buildArea(int xS, int yS, int ranSize){
+    int width = (2 * ((int) (Math.random() * 4)) + ranSize);
+    int length = (2 * ((int) (Math.random() * 4)) + ranSize);
     System.out.println("" + length + " " + width);
     if (xS - (length - 1) / 2 < 0 || xS + (length - 1) / 2 > tileMap[0].length ||
         yS - (width - 1) / 2 < 0 || yS + (width - 1) / 2 > tileMap.length){
           return false;
         }
     for(int x = xS - ((length - 1) / 2); x <= xS + ((length - 1) / 2) ; x++){
-      tileMap[yS - ((width - 1) / 2)][x].setColor(2);
+      if(tileMap[yS - ((width - 1) / 2)][x].getColor() != 0){
+        tileMap[yS - ((width - 1) / 2)][x].setColor(2);
+      }
       tileMap[yS + ((width - 1) / 2)][x].setColor(2);
     }
     for(int y = yS - (width - 1) / 2; y <= yS + (width - 1) / 2 ; y++){
