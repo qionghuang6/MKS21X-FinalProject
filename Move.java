b5e649d356422353f5a9d76fb20887e3acdb4b8b
Bryan Lai <bryanjl22@hotmail.com>
2019-01-03 22:40:41 -0500

    Create Move.java and define instance variables

---
+++ Move.java
@@ -0,0 +1,10 @@
+public class Move {
+        //Instance Variables:
+        private String name;
+        private String type;
+        private int pp;
+        private int levelRequirement;
+        private int baseDamage;
+
+
+}
