e59720207e96d5a6d60ecb8097e22a44cfcbbc84
qionghuang6 <qhuang00@stuy.edu>
2019-01-12 19:19:04 -0500

    added Moves to pokemonrandomizer class

---
+++ PokemonRandomizer.java
@@ -0,0 +1,14 @@
+public class PokemonRandomizer{
+  //Normal Moves
+  Move growl = new Move("Growl", "Normal",10,1,25);
+  Move tailWhip = new Move("Tail Whip", "Normal",15,1,35);
+  Move tackle = new Move("Growl", "Normal",20,1,40);
+  Move defenseCurl = new Move("Growl", "Normal",5,1,10);
+  //Fire moves
+  Move ember = new Move("Ember","Fire",20,1,50);
+  Move flameWheel = new Move("Flame Wheel","Fire",25,5,100);
+  //Water Moves
+  Move waterGun = new Move("Water Gun", "Water",30,10,100);
+  Move bubble = new Move("Bubble", "Water",15,5,50);
+  Move whirlpool = new Move("Whirlpool", "Water",10,1,25);
+}
